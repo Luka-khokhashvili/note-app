e89a1b78493f50c4d58f2dd262da7a1de7f7b280
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-03-13 15:20:22 +0400

    Enhance Sidebar Component and Update Index - Updated `sidebar.component.html` to improve UI/UX. - Updated `note box` to have limited description for better readability and look. - Note that `note box` is still non functional and will be updated in future commits. - Refactored `sidebar.component.ts` for better performance and readability. - Modified `index.html` to include new sidebar changes and improve overall structure.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -17,13 +17,55 @@ export class SidebarComponent {
     },
     {
       name: 'test2',
-      description: 'test2 description',
+      description:
+        'lorem impsum dolor sit amet consectetur adipiscing elit sed do eiusmod',
       tag: 'test2 tag',
     },
     {
       name: 'test3',
-      description: 'test3 description',
+      description:
+        'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt culpa officia deserunt mollit anim id est laborum',
       tag: 'test3 tag',
+    },
+    {
+      name: 'test4',
+      description: 'test4 description',
+      tag: 'test4 tag',
+    },
+    {
+      name: 'test5',
+      description: 'test5 description',
+      tag: 'test5 tag',
+    },
+    {
+      name: 'test6',
+      description: 'test6 description',
+      tag: 'test6 tag',
+    },
+    {
+      name: 'test7',
+      description: 'test7 description',
+      tag: 'test7 tag',
+    },
+    {
+      name: 'test8',
+      description: 'test8 description',
+      tag: 'test8 tag',
+    },
+    {
+      name: 'test9',
+      description: 'test9 description',
+      tag: 'test9 tag',
+    },
+    {
+      name: 'test10',
+      description: 'test10 description',
+      tag: 'test10 tag',
+    },
+    {
+      name: 'test11',
+      description: 'test11 description',
+      tag: 'test11 tag',
     },
   ];
 }
