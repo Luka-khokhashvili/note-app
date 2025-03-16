76fe7a8d9beecc00af2bab9b52f45aeea9223116
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-03-16 12:54:19 +0400

    Inject ngx-quill editor in note-box component   - Added Quill editor for better note editing experience   - Includes toolbar for basic text formatting   - Needs further testing and styling

--- src/app/components/note-box/note-box.component.ts
+++ src/app/components/note-box/note-box.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 
 @Component({
   selector: 'app-note-box',
-  imports: [],
+  imports: [QuillModule, FormsModule],
   templateUrl: './note-box.component.html',
-  styleUrl: './note-box.component.css'
+  styleUrl: './note-box.component.css',
 })
 export class NoteBoxComponent {
+  noteContent: string = '';
 
+  quillModules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'], // Basic formatting
+      [{ header: [1, 2, 3, false] }], // Headings
+      [{ size: ['small', false, 'large', 'huge'] }], // Font sizes
+      [{ color: [] }, { background: [] }], // Text & background color
+      [{ list: 'ordered' }, { list: 'bullet' }], // Lists
+      [{ indent: '-1' }, { indent: '+1' }], // Indent
+      [{ align: [] }], // Text alignment
+      ['link', 'image', 'video'], // Links, images, and videos
+      ['blockquote', 'code-block'], // Quotes & code blocks
+      ['clean'], // Clear formatting
+    ],
+  };
 }
